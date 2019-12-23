2cbfaaaf45dca38f76051db7fda57ef437a36b02
john-arul1 <34236264+john-arul1@users.noreply.github.com>
2019-12-23 18:29:53 +0530

    Add files via upload

--- unfold_maxlike.py
+++ unfold_maxlike.py
@@ -59,7 +59,7 @@ def Poisson(x,R,a):
     return res
 
 
-def L(x,R,a,phi,svphi):
+def L(x,R,a,phi):
     res=0
     #     Likelyhood +      dist to calculated phi+ dist to SVD phi
     res+=Poisson(x,R,a)+dist(x,phi)
@@ -67,8 +67,8 @@ def L(x,R,a,phi,svphi):
 
 
 ## read files
-def read_data(fname):    
-    
+def read_data(fname):
+
     fi=open(dict_fname[fname],"r")
     fi.readline() #skip  title
     # two column or one column file
@@ -79,22 +79,26 @@ def read_data(fname):
     n=int(n)
     data=fi.readlines()
     fi.close()
+
     m1=len(data)
     if m != m1:
         print("possible error in  input data")
-    
+
     B=zeros([m,1])
     i=0
     for line in data:
         line=line.rstrip()
         if n == 2:
-            
-            sn,B[i,0]=re.split('\t|,|\n| ',line)
+
+            sn,B[i,0]=re.split('\s+',line)  # any white space
             #sn,B[i,0]=line.split("\t",)
         else:
            B[i,0]=line
         i+=1
-    return B    
+        if i == m:  # in case files have blank lines
+           break
+
+    return B
 #------
 
 ###main driver
@@ -102,7 +106,7 @@ def read_data(fname):
 # assumes  minimum 2 input files are given, response and counts
 # further files are theoretical flux, guess flux
 # there can be standard files of guess flux and theoretical flux
-# this is standard flux files have to be implemented    
+# this is standard flux files have to be implemented
 
 flag_theory=0
 flag_guess=0
@@ -124,15 +128,16 @@ lines=file_list.readlines()
 dict_fname={}
 for line in lines:
     line=line.rstrip()
+    if line=='end':
+        break
     print (line)
     key,fname=line.split(',')
     dict_fname.update({key:fname})
 
-
 if 'response' not in dict_fname:
     print ("Error response matrix file not specified")
     sys.exit(1)
-    
+
 if "counts" not in  dict_fname:
     print ("Measured counts file not specified")
     sys.exit(1)
@@ -142,20 +147,21 @@ f1=open(dict_fname["response"],"r")
 f1.readline() #title
 dim_txt=f1.readline()
 dim_txt=dim_txt.rstrip()
-#assumes flux x measurement matrix, col number of energy vector 
+#assumes flux x measurement matrix, col number of energy vector
 # E is  first or last coloumn
-n,m,Ecol=dim_txt.split(',')
+n,m,Ecol,unit=dim_txt.split(',')
 n,m,Ecol=map(int,[n,m,Ecol])
 
 resp_data=f1.readlines()
 f1.close()
+
 n1=len(resp_data)
 
 if n1 != n:
     print("response data input error")
     #n=n1   # stll can proceed
-    
-    
+
+
 Rinp=zeros([n,m])
 i=0
 for line in resp_data:
@@ -169,14 +175,14 @@ if Ecol ==1:
 elif Ecol==m:
     R=Rinp[:,:m-1].transpose()  #response matrix
 
-E=np.copy(Rinp[:,0])    
+E=np.copy(Rinp[:,0])
 E=list(map(float,E))
 
 if 'counts' in dict_fname:
     # read teoretical flux
     CT=read_data('counts')
     B=np.copy(CT[:,0])
-    
+
 if 'theory' in dict_fname:
     # read teoretical flux
     TH=read_data('theory')
@@ -203,14 +209,14 @@ X=R.T.dot(R)
 X=X+np.identity(52)*rp
 XI=np.linalg.inv(X)
 x1=XI.dot(R.T.dot(B))
-
+x1s=x1
 ##---------------------------------
 
 
 if not flag_theory:
-    
+
     plt.semilogx(E,x1,label="Regularized")
-    plt.xlabel("Energy bin (eV)")
+    plt.xlabel("Energy bin ("+unit+")")
     plt.ylabel(" flux (n/sq.cm/s)")
     plt.grid()
     bn1=R.dot(x1)
@@ -219,6 +225,8 @@ if not flag_theory:
 
 else:
 
+
+ # R . phics = B
     cp=R.dot(phic)
     scale=B.dot(B)/(B.dot(cp))
 
@@ -226,61 +234,78 @@ else:
     flnorm=sum(phics)
     phicn=phics/flnorm
     Bn=B/flnorm
-    
-# xinit options  i) guess phi0 ii) from regularisation iii) theory
-# if guess is given use it, else chack for theoretical value, else default is phi0 
 
-    x1s=x1*scale
-    x1n=x1s/flnorm
-    
+# xinit options  i) guess phi0 ii) from regularisation iii) theory
+# if guess is given use it, else chack for theoretical value, else default is phi0
+
+
+    x1n=x1/flnorm
+
+
     if flag_guess:
         xinit=phi0n
     elif flag_theory:
         xinit=phicn
     else:
-        xinit=x1n
+        xinit=phicn#x1n
 
 
     ##------------------------------------------------- use SLSQP
     bnds=tuple([(0.0, None) for l in range(len(phic))])
     #cons=({'type': 'eq', 'fun': lambda x:am.dot(x)-m})
     cons=({'type': 'eq', 'fun': lambda x:constr_norm(x)})
-    
+
     opt={'eps': 1.0E-2, 'maxiter': 1000, 'ftol': 1E-12}
     #opt={'maxiter': 1000, 'ftol': 1e-10}
     #res = minimize(Entro,x0=phicn, args=(phicn),method='SLSQP',bounds=bnds,constraints=cons,options=opt)
-    
+
     res = minimize(L,x0=xinit,args=(R,Bn,phicn),method='SLSQP',bounds=bnds,constraints=cons,options=opt)
-    x2s=res.x*flnorm/scale
+    x2s=res.x*flnorm
     ###-------------------------------------------------
-    
-    
+
+
     '''
     Plot and save
     '''
-    
+
     handle=plt.semilogx(E,x1s,'+',E,x2s,'-',E,phics,'--')
     ax=plt.gca()
     ax.legend(handle,["Regularized","MaxLikelihood","Theory"])
     #E=[1,2,3,4,5]
-    plt.xlabel("Energy bin (eV)")
+    plt.xlabel("Energy bin ("+unit+")")
     plt.ylabel(" flux (n/sq.cm/s)")
     #plt.legend()
-    
-    
+
+
     plt.grid()
     plt.savefig("unfold_.png")
     #ax.legend(handle1,["ML algorithm"])
-        
-    bn1=R.dot(x1s*scale/flnorm)
-    bnreg=R.dot(x2s*scale/flnorm)
-    error=(bn1-Bn)/Bn*100.0
-    err_reg=(bnreg-Bn)/Bn*100.0
-    
+
+    bn1=R.dot(x2s)
+    bnreg=R.dot(x1s)
+    error=(bn1-B)/B*100.0
+    err_reg=(bnreg-B)/B*100.0
+
     print ("error%=",error,"\n",err_reg)
 
+    fl=open('result_flux.txt','w')
 
-'''    
+    fl.write("Energy bin ("+unit+")\t Flux in n/cm2/s\n")
+    for i in range(len(E)):
+        fl.write("{:f},{:.3E}\n".format(E[i],x2s[i]))
+    fl.close()
+
+    erf=open('result_error.txt','w')
+    erf.write("Percent Error between measured and predicted count")
+    erf.write("CPS - Measured,predicted,regularised,error(pre-m)/m,error(reg-m)/m")
+    for i in range(len(B)):
+        l1=[B[i],bn1[i],bnreg[i],error[i],err_reg[i]]
+        for j in l1:
+            erf.write("{:.3f}\t".format(j))
+        erf.write("\n")
+    erf.close()
+
+'''
     hdle=plt.plot(x,amber,'o',x,ambers,'x',x,ambersvd,'+')
     ax=plt.gca()
     ax.legend(hdle,["without entropy","with entropy","svd result"])
@@ -288,10 +313,6 @@ else:
     plt.ylabel("percent error in predcited counts")
     plt.grid()
     plt.savefig("error_ambe2.png")
-
-    A=np.array([[1.0,1.0],[0.0,2]])
-    U,s,VT=svd(A)
-
 
 '''
 
